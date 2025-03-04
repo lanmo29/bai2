640595b159121a7a09eaac201a9cafef67561969
lanmo29 <hungmo29072003@gmail.com>
2025-03-05 03:06:21 +0700

    Add files via upload

---
+++ src/main/java/com/example/bai2/doituong.java
@@ -0,0 +1,30 @@
+package com.example.bai2;
+
+public class doituong {
+    private String hoten, mssv;
+    private Long SOTAIKHOAN;
+
+    public String getHoten() {
+        return hoten;
+    }
+
+    public void setHoten(String hoten) {
+        this.hoten = hoten;
+    }
+
+    public String getMssv() {
+        return mssv;
+    }
+
+    public void setMssv(String mssv) {
+        this.mssv = mssv;
+    }
+
+    public Long getSOTAIKHOAN() {
+        return SOTAIKHOAN;
+    }
+
+    public void setSOTAIKHOAN(Long SOTAIKHOAN) {
+        this.SOTAIKHOAN = SOTAIKHOAN;
+    }
+}
